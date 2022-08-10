2bfa0cda7e943771b5d6a5fa989956c7a5b8dec5
Khushiyant <khushiyant2002@gmail.com>
2022-08-10 15:35:06 +0530

    serializer: add user serializer for auth

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
